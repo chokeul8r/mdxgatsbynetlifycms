8ee1c6722584f8de404708644c8787ee4886288c
Mark <mark.long@marklong.io>
2020-01-11 16:18:01 -0700

    Changed index.md reflect new data and data types

--- src/page-templates/home-page.template.js
+++ src/page-templates/home-page.template.js
@@ -1,28 +1,41 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import { Heading, SEO } from "../components"
 import { RenderMarkdown } from "../core"
 import Layout from "../app-layout.component"
 import {
   safelyGetFrontMatter,
-  withFallback,
+  // withFallback,
 } from "../cms"
 
-export const HomePageTemplate = ({ title, sections }) => (
+export const HomePageTemplate = ({
+  title,
+  featuredImage,
+  headline,
+  subHeadline,
+  introduction,
+  callToAction,
+  profileImage,
+  body,
+}) => (
   <Layout>
     <SEO title={title} />
     <Heading tag={1}>{title}</Heading>
-    {withFallback(sections, []).map((section, i) => {
-      return (
-        <section key={i}>
-          <h2>{section.title}</h2>
-          <RenderMarkdown
-            md={section.body}
-          />
-          <hr />
-        </section>
-      )
-    })}
+    <div style={{ maxWidth: `1500px`, marginBottom: `1.45rem` }}>
+      <img src={featuredImage} alt="background" />
+    </div>
+    <h1>{headline}</h1>
+    <h2>{subHeadline}</h2>
+    <p>{introduction}</p>
+    <RenderMarkdown md={body} />
+    <div>
+      <button>{callToAction}</button>
+    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <img src={profileImage} alt="Ryan" />
+    </div>
+    <Link to="/thank-you/">Go to the Thank You Page</Link>
   </Layout>
 )
 
