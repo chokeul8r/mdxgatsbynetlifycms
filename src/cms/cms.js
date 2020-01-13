7c8b4c54aaa8e7ca4e81c51311886a46b6f92290
Mark <mark.long@marklong.io>
2020-01-12 23:04:13 -0700

    Reinstalled Font Picker Plugin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,7 @@ import CMS from "netlify-cms-app"
 
 import { HomePagePreview, ThankYouPagePreview } from "./preview-templates"
 import { ctaEditorConfig } from "./editor-components"
-// import NetlifyCmsWidgetFontpicker from "netlify-cms-widget-fontpicker"
+import NetlifyCmsWidgetFontpicker from "netlify-cms-widget-fontpicker"
 // import * as ColorWidget from "netlify-cms-widget-color"
 // import * as FontawesomeWidget from "netlify-cms-widget-fontawesome"
 
@@ -12,7 +12,7 @@ CMS.registerPreviewTemplate("thank-you", ThankYouPagePreview)
 
 // Extend editor
 CMS.registerEditorComponent(ctaEditorConfig)
-// CMS.registerWidget([NetlifyCmsWidgetFontpicker.Widget()])
+CMS.registerWidget([NetlifyCmsWidgetFontpicker.Widget()])
 // CMS.registerWidget("color", ColorWidget.Control)
 // CMS.registerWidget(
 //   "fontawesome",
