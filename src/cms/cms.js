843d20dc6a9af13ad06b57a6d17d5f77f334aa01
Mark <mark.long@marklong.io>
2020-01-12 22:19:19 -0700

    RM Color Widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,7 @@ import CMS from "netlify-cms-app"
 import { HomePagePreview, ThankYouPagePreview } from "./preview-templates"
 import { ctaEditorConfig } from "./editor-components"
 import NetlifyCmsWidgetFontpicker from "netlify-cms-widget-fontpicker"
-import * as ColorWidget from "netlify-cms-widget-color"
+// import * as ColorWidget from "netlify-cms-widget-color"
 import * as FontawesomeWidget from "netlify-cms-widget-fontawesome"
 
 // Add Previews
@@ -13,5 +13,9 @@ CMS.registerPreviewTemplate("thank-you", ThankYouPagePreview)
 // Extend editor
 CMS.registerEditorComponent(ctaEditorConfig)
 CMS.registerWidget([NetlifyCmsWidgetFontpicker.Widget()])
-CMS.registerWidget("color", ColorWidget.Control)
-CMS.registerWidget("fontawesome", FontawesomeWidget.Solid, FontawesomeWidget.Preview )
+// CMS.registerWidget("color", ColorWidget.Control)
+CMS.registerWidget(
+  "fontawesome",
+  FontawesomeWidget.Solid,
+  FontawesomeWidget.Preview
+)
