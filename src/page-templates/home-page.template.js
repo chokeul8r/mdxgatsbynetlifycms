06d26267a08a0e508613fa2e2ba1dcd08a630560
Mark <mark.long@marklong.io>
2020-01-14 16:42:00 -0700

    Updated Background Styles

--- src/page-templates/home-page.template.js
+++ src/page-templates/home-page.template.js
@@ -4,7 +4,7 @@ import { Heading, SEO } from "../components"
 import { RenderMarkdown } from "../core"
 import { safelyGetFrontMatter, withFallback } from "../cms"
 import ModalPopUp from "../components/modal"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Jumbotron, Image } from "react-bootstrap"
 
 export const HomePageTemplate = ({
   title,
@@ -17,31 +17,33 @@ export const HomePageTemplate = ({
   return (
     <>
       <SEO title={title} />
-      <Container className="mt-5 mb-5 pt-5 pb-3">
-        <Row>
-          <Col sm={12} lg={6} className="px-1">
-            {/* <div style={{ maxWidth: `1500px`, marginBottom: `1.45rem` }}>
-              <img src={featuredImage} alt="background" />
-            </div> */}
-            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-              <img src={profileImage} alt="Ryan" />
-            </div>
-          </Col>
-          <Col sm={12} lg={6} className="px-1">
-            <Heading tag={1}>{headline}</Heading>
-            <Heading tag={4}>{subHeadline}</Heading>
-            {withFallback(sections, []).map((section, i) => {
-              return (
-                <section key={i}>
-                  <RenderMarkdown md={section.body} />
-                </section>
-              )
-            })}
-            <ModalPopUp />
-          </Col>
-          <Link to="/thank-you/">Go to the Thank You Page</Link>
-        </Row>
-      </Container>
+      {/* <div style={{ maxWidth: `1500px`, marginBottom: `1.45rem` }}>
+      <img src={featuredImage} alt="background" />
+    </div> */}
+      <Jumbotron className="bg-secondary m-0">
+        <Container className="mt-5 mb-4 ">
+          <Row>
+            <Col sm={12} lg={6} className="px-1">
+              <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Image src={profileImage} alt="placeholder" roundedCircle  />
+              </div>
+            </Col>
+            <Col sm={12} lg={6} className="px-1">
+              <Heading tag={1}>{headline}</Heading>
+              <Heading tag={4}>{subHeadline}</Heading>
+              {withFallback(sections, []).map((section, i) => {
+                return (
+                  <section key={i}>
+                    <RenderMarkdown md={section.body} />
+                  </section>
+                )
+              })}
+              <ModalPopUp />
+            </Col>
+            <Link to="/thank-you/">Go to the Thank You Page</Link>
+          </Row>
+        </Container>
+      </Jumbotron>
     </>
   )
 }
